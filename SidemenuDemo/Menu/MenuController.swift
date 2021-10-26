e73d3a1dc1c643081e96f940b849773e26c36821
Ravi Yaganti <raviy@backbase.com>
2021-10-26 19:34:53 +0530

    Testing

--- SidemenuDemo/Menu/MenuController.swift
+++ SidemenuDemo/Menu/MenuController.swift
@@ -30,8 +30,8 @@ class MenuController: UITableViewController {
     
     lazy var headerView: UIView = {
         let view = UIView(frame: .zero)
-//        view.layer.borderWidth = 0.5
-//        view.layer.borderColor = UIColor.lightGray.cgColor
+        view.layer.borderWidth = 0.5
+        view.layer.borderColor = UIColor.lightGray.cgColor
         view.backgroundColor = UIColor.systemGroupedBackground.withAlphaComponent(0.4)
         view.translatesAutoresizingMaskIntoConstraints = false
         
